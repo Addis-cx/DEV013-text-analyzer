bbe156c2fbe29029accf45d48a621f657cc53f7f
Addis <lumiaddis@gmail.com>
2024-01-09 19:22:06 -0300

    Corregí los errores que me mostraban los test junto con hacer el README

--- src/analyzer.js
+++ src/analyzer.js
@@ -12,25 +12,41 @@ const analyzer = {
     return Number(contadorCCE);
   },
   getAverageWordLength: (textUsuario) => {    
-    const contadorAWL = textUsuario.split(/\s+/);
+    const contadorAWL = textUsuario.split(/\s+/g);
     let longitudP = 0;
     for (let i = 0; i < contadorAWL.length; i++) {
       longitudP += contadorAWL[i].length;
     }
-    return longitudP/contadorAWL.length;
+    return parseFloat(Number(longitudP/contadorAWL.length).toFixed(2));
   },
   getNumberCount: (textUsuario) => {
-    const contadorNC = textUsuario.match(/\d+/g);
-    if (contadorNC) {
-      return contadorNC.length;
-    } 
-    else {
+    const contadorNC = textUsuario.match(/\b\d+(\.\d+)?\b/g);
+    let sumaNumero = 0;
+    if (contadorNC === null) {
       return 0;
     }
+    else {
+      for (let i = 0; i < contadorNC.length; i++) {
+        sumaNumero++;
+      }
+    }
+    return sumaNumero;
   },
   getNumberSum: (textUsuario) => {
-  
+    const contadorNS = textUsuario.match(/\b\d+(\.\d+)?\b/g);
+    let sumaNumero = 0;
+    if (contadorNS === null) {
+      return 0;
+    }
+    else {
+      for (let i = 0; i < contadorNS.length; i++) {
+        const numero = contadorNS[i];
+        sumaNumero = sumaNumero + parseFloat(numero);
+      }
+    }
+    return sumaNumero;
   },
+  
 };
 
 export default analyzer;
